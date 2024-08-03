5ab3c66b099327075f0d5330e874b8a1e553ddc6
robMolloy <robmolloy@hotmail.co.uk>
2024-08-03 13:43:19 +0100

    home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -147,6 +147,77 @@ const Parent = () => {
             </li>
           </ul>
         </div>
+        <br />
+        <br />
+        <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
+          <div>
+            <h2 className="mt-0">Practicalities</h2>
+            <p>
+              During our initial meeting, we'll agree the best way to work together taking into
+              account your goals, work schedule, learning and communication preferences.
+            </p>
+            <p>
+              Although I believe the best way to coach is face to face, this can be in person, or
+              via Zoom. Walking coaching is very powerful, being out in nature helps us to free our
+              mind from the usual constraints; it's all about what works best for you.
+            </p>
+            <p>
+              Coaching is usually time limited and focused on achieving specific goals and outcomes.
+              This normally takes 4-6 sessions depending on the complexity of the outcomes you want
+              to achieve. We can amend this as the sessions develop, if you achieve your outcomes in
+              the first session, that's great!
+            </p>
+            <p>
+              Coaching is a powerful development tool that we can all benefit from; I want to ensure
+              my coaching is accessible to everyone and charge a lower rate if you are self-funding
+              or work for a charity.
+            </p>
+            <p>Contact me for a no obligation discussion</p>
+          </div>
+          <div
+            style={{
+              backgroundImage: "url('/img/myFlowers.jpg')",
+              backgroundSize: "100% auto",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+          />
+        </div>
+        <br />
+        <br />
+        <div className="rounded-box bg-neutral p-4 text-neutral-content">
+          <h2 className="mt-0 text-center text-neutral-content">Testimonials</h2>
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
+            <div>
+              <h3 className="mt-0 text-center text-neutral-content">Robert</h3>
+              <p>
+                Janette's coaching had a profound effect on me both personally and professionally,
+                Over the course of our sessions, Janette not only gave great and practical advice,
+                she listened and provided a superb mirror for the areas I needed to take
+                responsibility for.
+              </p>
+              <p className="mb-0">
+                The impact of this was not only an improvement in my performance as a leader, but
+                also it was a huge boost to our entire organisation. I would recommend Janette to
+                anyone wanting to make a positive change in their life.
+              </p>
+            </div>
+            <div>
+              <h3 className="mt-0 text-center text-neutral-content">Hannah</h3>
+              <p>
+                I was lucky enough to have personal coaching with Janette. Janette was not only a
+                great coach professionally but personally helped me with tough decisions. The
+                discussions we had during coaching helped me develop alternate ways of thinking and
+                leading our relatively new team into our most profitable year in a decade.
+              </p>
+              <p className="mb-0">
+                The time given to coaching enabled me to take a step back, invest time in myself,
+                and in turn, gave the team a boost in their own leadership capabilities. Coaching
+                with Janette was most definitely my best professional investment.
+              </p>
+            </div>
+          </div>
+        </div>
       </Typography>
     </main>
   );
